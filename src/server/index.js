eadf39508a0d9dbe47b110bc55aef639453c9ba0
20210770 <punnlert@kaist.ac.kr>
2024-10-26 19:27:06 +0900

    it works

---
+++ src/server/index.js
@@ -0,0 +1,69 @@
+import express from "express";
+import "dotenv/config";
+import { getCollection, retrieveText } from "./rag_utils.js";
+import { getPrompt } from "./ai_utils.js";
+
+const PORT = 3000;
+const app = express();
+
+const prompt = getPrompt([
+  {
+    role: "system",
+    content: `You are an experienced developer, expert at
+interpreting and answering questions based on provided documentation.
+Using the provided context, answer the user's question to the
+best of your ability using only the resources provided. Be sure
+to provide a clear and concise answer. Do not mention the
+provided context in your answer. Be succinct and say "I do not
+know" if you do not know`,
+  },
+]);
+
+let text_docs;
+let code_docs;
+
+app.use(express.json());
+
+app.post("/", (req, res) => {
+  console.log(req.body);
+  res.send("hello");
+});
+
+app.post("/fetchDocs", async (req, res) => {
+  console.log(req.body);
+  const { owner, repo, branch } = req.body;
+  const { text_collection, code_collection } = await getCollection(
+    owner,
+    repo,
+    branch,
+  );
+  text_docs = text_collection;
+  code_docs = code_collection;
+  res.send("complete");
+});
+
+app.post("/askQuestion", async (req, res) => {
+  const { question } = req.body;
+  // console.log(text_docs, code_docs);
+  const writing_context = await retrieveText(text_docs, `${question}`, 12);
+  const coding_context = await retrieveText(code_docs, `${question}`, 4);
+  // console.log(writing_context, coding_context);
+
+  const aiResponse = await prompt(
+    `Answer this question <question> ${question} <question> 
+    using this context
+    <context> ${writing_context} <context>
+    to answer it in terms of general understanding
+    and use this coding context
+    <coding_context> ${coding_context} <coding_context>
+    to answer it when coding snippets are required.`,
+  );
+  console.log(aiResponse);
+  res.status(200).send(aiResponse.content);
+});
+
+app.listen(PORT, () => {
+  console.log(
+    `[SERVER] ExpressJS is listening to port http://localhost:${PORT}]`,
+  );
+});
---
+++ src/server/rag_utils.js
@@ -0,0 +1,156 @@
+import axios from "axios";
+import "dotenv/config";
+import { RecursiveCharacterTextSplitter } from "langchain/text_splitter";
+import { Document } from "langchain/document";
+import { OpenAIEmbeddingFunction, ChromaClient } from "chromadb";
+
+const code_extension = ["py", "yml", "js"];
+const skip_extension = ["zip"];
+
+export async function getCollection(
+  githubOwner,
+  githubRepo,
+  githubBranch = "main",
+) {
+  const name = [githubOwner, githubRepo, githubBranch].join("");
+  const client = new ChromaClient("http://localhost:8000");
+
+  const embeddingFunction = new OpenAIEmbeddingFunction({
+    model: "text-embedding-3-small",
+    encoding_format: "float",
+    openai_api_key: process.env.OPENAI_API_KEY,
+  });
+
+  let text_collection;
+  let code_collection;
+  try {
+    text_collection = await client.getCollection({
+      name: `${name}-text`,
+      embeddingFunction,
+    });
+
+    code_collection = await client.getCollection({
+      name: `${name}-code`,
+      embeddingFunction,
+    });
+  } catch (error) {
+    const { text_document, code_document } = await loadAndSplitDocuments(
+      githubOwner,
+      githubRepo,
+      githubBranch,
+    );
+    // console.log(text_document, code_document);
+
+    if (text_document == undefined || code_document == undefined) {
+      throw new Error("not getting code or text");
+    }
+
+    text_collection = await client.createCollection({
+      metadata: { "hnsw:space": "cosine" },
+      name: `${name}-text`,
+      embeddingFunction,
+    });
+
+    code_collection = await client.createCollection({
+      metadata: { "hnsw:space": "cosine" },
+      name: `${name}-code`,
+      embeddingFunction,
+    });
+
+    await vectorIngestion(text_collection, text_document);
+    await vectorIngestion(code_collection, code_document);
+  }
+  return { text_collection, code_collection };
+}
+
+async function loadAndSplitDocuments(githubOwner, githubRepo, githubBranch) {
+  const githubToken = process.env.GITHUB_TOKEN;
+
+  // Step 1: Get all files in the GitHub repository
+  const url = `https://api.github.com/repos/${githubOwner}/${githubRepo}/git/trees/${githubBranch}?recursive=1`;
+
+  const headers = githubToken ? { Authorization: `token ${githubToken}` } : {};
+
+  try {
+    const response = await axios.get(url, { headers });
+    const files = response.data.tree.filter((item) => item.type === "blob");
+
+    const writing_context = [];
+    const coding_context = [];
+
+    // Step 2: Get content of each file then create documents
+    for (const file of files) {
+      const path = file.path;
+      const type =
+        path.split(".").length >= 2
+          ? path.split(".")[path.split(".").length - 1]
+          : "other";
+
+      const fileUrl = `https://api.github.com/repos/${githubOwner}/${githubRepo}/contents/${path}`;
+
+      try {
+        const fileResponse = await axios.get(fileUrl, { headers });
+        const content = fileResponse.data.content;
+
+        if (!content) {
+          console.error(`No content for file ${path}`);
+          continue;
+        }
+
+        const decodedContent = Buffer.from(content, "base64").toString("utf-8");
+
+        // Create a document with the file content
+        const doc = new Document({
+          pageContent: decodedContent,
+          metadata: { source: file.path },
+        });
+
+        if (code_extension.includes(type)) {
+          coding_context.push(doc);
+        } else {
+          writing_context.push(doc);
+        }
+      } catch (err) {
+        console.error(`Error fetching file ${file.path}:`, err.message);
+      }
+    }
+
+    // Step 3: Split the documents using the splitter
+    const splitter = new RecursiveCharacterTextSplitter({
+      chunkSize: 2048,
+      chunkOverlap: 512,
+    });
+
+    const writing_data = await splitter.splitDocuments(writing_context);
+    const coding_data = await splitter.splitDocuments(coding_context);
+    const data = { text_document: writing_data, code_document: coding_data };
+    return data;
+  } catch (err) {
+    console.error("Error fetching repository contents:", err.message);
+    return { text_document: [], code_document: [] };
+  }
+}
+
+export async function retrieveText(collection, question, nResults = 4) {
+  const result = await collection.query({
+    queryTexts: question,
+    nResults,
+  });
+  console.log(result.documents);
+  const context = result.documents.join(" ");
+  return context;
+}
+
+async function vectorIngestion(collection, docs) {
+  docs.forEach(async (document, i) => {
+    console.log(document);
+    await collection.add({
+      ids: [`${i}`],
+      documents: [
+        `the below document ${i} is from ${document.metadata.source} path\n` +
+          `<document ${i}>\n ${document.pageContent}\n </document ${i}>`,
+      ],
+      metadatas: [document.metadata],
+    });
+  });
+}

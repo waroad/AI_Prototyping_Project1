ece7938cdbb3a1a042b68e706aeb8a6539264cc3
20210770 <punnlert@kaist.ac.kr>
2024-10-27 22:34:57 +0900

    clean up preparing to integrate with extension

--- chrome-gpt-extension-main/extension/background.js
+++
@@ -1,9 +0,0 @@
-// background.js
-chrome.runtime.onInstalled.addListener(() => {
-    console.log("Extension installed");
-});
-
-chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
-    console.log("Received message in background:", message);
-    sendResponse({ response: "Background received the message" });
-});
